aedad172b22283f500d15f51441295450da6d29c
YAikawa0822 <yuya.aikawa@map4.jp>
2025-06-30 16:19:11 +0900

    tiny fix

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker run --name potree-server-test -d\
+ -p 9999:80\
+ -v "$(pwd)/nginx/default.conf":/etc/nginx/conf.d/default.conf\
+ -v /mnt/bigdata/00_students/aichi_ucl/potree-server-data/meidai:/usr/share/nginx/potree/meidai\
+ -v /mnt/bigdata/00_students/aichi_ucl/potree-server-data/trusco:/usr/share/nginx/potree/trusco\
+ potree-server
